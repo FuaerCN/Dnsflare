12f5af0eba2e80d0377cfc1fed5711399d00aeb2
FuaerCN <FuaerCN@Gmail.com>
2025-04-13 23:28:17 +0800

    Create security.ts

---
+++ src/api/security.ts
@@ -0,0 +1,50 @@
+import { useAxios } from '@/utils/requests'
+import { objectToCamelCase, objectToHungarian } from '@/utils/case'
+import { APIResponse } from '.'
+
+export type SecurityLevel = 'off' | 'essentially_off' | 'low' | 'medium' | 'high' | 'under_attack'
+
+export const SecurityLevelDisplay: Record<SecurityLevel, string> = {
+    off: '关闭',
+    essentially_off: '基本关闭',
+    low: '低',
+    medium: '中',
+    high: '高',
+    under_attack: 'Under Attack模式'
+}
+
+export type CloudflareSecurityInfo = {
+    securityLevel: SecurityLevel
+}
+
+export async function getSecurityInfo(zoneId: string): Promise<APIResponse<CloudflareSecurityInfo>> {
+    const axios = useAxios()
+
+    try {
+        const response = await axios.request<any>({
+            url: `/zones/${zoneId}/settings/security_level`,
+            method: 'get',
+        })
+
+        return (objectToCamelCase(response.data) as any)
+    } catch (err) {
+        return (objectToCamelCase(err.response.data) as any)
+    }
+}
+
+export async function updateSecurityLevel(zoneId: string, level: SecurityLevel): Promise<APIResponse<any>> {
+    const axios = useAxios()
+    try {
+        const response = await axios.request<any>({
+            url: `/zones/${zoneId}/settings/security_level`,
+            method: 'patch',
+            data: objectToHungarian({
+                value: level
+            })
+        })
+
+        return (objectToCamelCase(response.data) as any)
+    } catch (err) {
+        return (objectToCamelCase(err.response.data) as any)
+    }
+}
